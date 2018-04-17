1cec2fdd1dbea119467704296cc172dd49d429f8
jorge perez <jorgeperez00@hotmail.com>
2018-04-17 17:44:02 -0400

    first commit

---
+++ ExcelExamples/ExcelCellFormatting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelExamples {
+    [TestClass]
+    public class ExcelCellFormatting {
+        [TestMethod]
+        public void ChangeBorderColor() {
+            // Open the document for editing.
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("ExcelFile\\Sample.xlsx", false)) {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                string text;
+                foreach (Row r in sheetData.Elements<Row>()) {
+                    foreach (Cell c in r.Elements<Cell>()) {
+                        text = c.CellValue.Text;
+                        Console.Write(text + " 111111111");
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ChangeCellBorder() {
+            // Open the document for editing.
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("ExcelFile\\Sample.xlsx", true)) {
+                // Code removed here.
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                string text;
+                foreach (Row r in sheetData.Elements<Row>()) {
+                    foreach (Cell c in r.Elements<Cell>()) {
+                        c.CellValue.Text = c.CellValue.Text + " 111111111";
+                    }
+                }
+                spreadsheetDocument.Save();
+            }
+        }
+
+        [TestMethod]
+        public void ChangeCellValueAndSave() {
+            // Open the document for editing.
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("ExcelFile\\Sample.xlsx", true)) {
+                // Code removed here.
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                string text;
+                foreach (Row r in sheetData.Elements<Row>()) {
+                    foreach (Cell c in r.Elements<Cell>()) {
+                        c.CellValue.Text = c.CellValue.Text + " 111111111";
+                    }
+                }
+                spreadsheetDocument.Save();
+            }
+        }
+
+        [TestMethod]
+        public void LoadExcelFile() {
+            // Open the document for editing.
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("ExcelFile\\Sample.xlsx", false)) {
+                // Code removed here.
+                Assert.IsNotNull(spreadsheetDocument);
+            }
+        }
+    }
+}
