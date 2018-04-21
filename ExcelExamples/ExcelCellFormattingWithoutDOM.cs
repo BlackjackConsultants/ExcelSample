3c76b440412b8e589574dbebbe3965a46486712e
jorge perez <jorgeperez00@hotmail.com>
2018-04-21 11:29:24 -0400

    added cell formatting without dom

---
+++ ExcelExamples/ExcelCellFormattingWithoutDOM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelExamples.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using X14 = DocumentFormat.OpenXml.Office2010.Excel;
+
+namespace ExcelExamples {
+    [TestClass]
+    public class ExcelCellFormattingWithoutDOM {
+        [TestMethod]
+        public void ChangeCellColor() {
+        }
+    }
+}
